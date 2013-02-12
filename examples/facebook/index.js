fbfa4f3e9a06ee9dd004f13aab5e810f66e66a39
Van Nguyen <thegoleffect@gmail.com>
2013-02-11 18:09:17 -0800

    jar debug code

--- examples/facebook/index.js
+++ examples/facebook/index.js
@@ -5,6 +5,25 @@ var FacebookStrategy = require('passport-facebook').Strategy
 
 var server = new Hapi.Server('localhost', 8000);
 
+// enable session cookie jar
+var cookieOptions = {
+    permissions: {
+        ext: true
+    },
+    plugin: {
+        name: 'jar',
+        isSingleUse: false,
+        options: {
+            password: 'worldofwalmart'
+        }
+    }
+};
+server.plugin().require('hapi-jar', cookieOptions, function (err) {
+    if (err) {
+        throw err;
+    }
+});
+
 
 /* Config.json in format of:
 
@@ -33,11 +52,54 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
+var getSession = function () {
 
-server.ext('onRequest', [
+    return function (request, next) {
+
+        // console.log('getSession sees: ', request.state.jar)
+        // if (request.state && request.state.jar && request.state.jar['passport']) {
+        //     request.session = JSON.parse(request.state.jar['passport']);
+        // }
+        // else {
+        //     request.session = {};
+        // }
+        // console.log('getting session:', request.session)
+        console.log('getSession state', request.state.jar)
+        console.log('getSession plugin', request.plugins.jar)
+        
+        next();
+    };
+};
+
+var saveSession = function () {
+    
+    return function (request, next) {
+
+        // console.log('saveSession sees: ', request.session)
+        // if (request.session) {
+        //     // request.plugins.jar = request.session;
+        //     request.plugins.jar['passport'] = JSON.stringify(request.session.passport);
+        // }
+        // console.log('saving jar:', request.plugins.jar)
+        console.log('saveSession state', request.state.jar)
+        console.log('saveSession plugin', request.plugins.jar)
+        console.log('adding to request.plugin')
+        request.plugins.jar['key'] = 'testing'
+        
+        next();
+    };
+};
+
+
+server.ext('onPreHandler', [
+    getSession(),
     passport.initialize(),
-    passport.session()
+    // passport.session()
 ]);
+
+server.ext('onPostHandler', [
+    // saveSession()
+])
 
 var ensureAuthenticated = function (request, next) {
 
@@ -60,7 +122,9 @@ var authFBCB = {
 
             // console.log('request._passport', request._passport)
             // console.log(util.inspect(request), null, 2)
-            request.reply("authenticated with request.user = ", request.user);
+            saveSession()(request, function(){
+                request.reply("authenticated with request.user = ", request.user);
+            });
         })
     }
 };
@@ -83,7 +147,8 @@ server.addRoute({
             request.reply('<a href="/auth/facebook">Login with Facebook</a>')
         }
     }
-})
+});
+
 
 server.addRoute({
     method: 'GET',
@@ -96,6 +161,51 @@ server.addRoute({
 
                 request.reply("Access Granted");
             });
+        }
+    }
+});
+
+server.addRoute({
+    method: 'GET',
+    path: '/setCookie/{val?}',
+    config: {
+        handler: function (request) {
+
+            request.plugins.jar = {
+                test: request.params.val || 1
+            }
+            
+            console.log('set: ', request.plugins, this.plugins)
+            console.log('session?', request.session)
+            saveSession()(request, function(){
+                request.reply('cookie set <a href="/getCookie">Click here</a> to see it.')
+            })
+            // request.reply('cookie set');
+            
+        }
+    }
+});
+
+server.addRoute({
+    method: 'GET',
+    path: '/getCookie',
+    config: {
+        handler: function (request) {
+
+            console.log('session?', request.session)
+            request.reply(request.state.jar);
+        }
+    }
+});
+
+server.addRoute({
+    method: 'GET',
+    path: '/clearCookie',
+    config: {
+        handler: function (request) {
+
+            request.plugins.jar = null;
+            request.reply(request.state.jar);
         }
     }
 });
